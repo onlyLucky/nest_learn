b76cd6cc8931a474f3b8c8bfe6d3ba3866f92944
feynman <2901954062@qq.com>
2023-01-01 10:54:53 +0800

    fix: 修复报错

--- basic/src/app.module.ts
+++ basic/src/app.module.ts
@@ -4,11 +4,10 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { DemoModule } from './demo/demo.module';
 import { UserModule } from './user/user.module';
-import { UserController } from './user/user.controller';
 
 @Module({
   imports: [UserModule, DemoModule],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
