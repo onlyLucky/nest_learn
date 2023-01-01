4b9b2b4d6cc7bee8bc5446a8dc7df66183cec409
feynman <2901954062@qq.com>
2023-01-01 11:33:21 +0800

    feat: 修改脚手架自定义生成文件

--- basic/src/app.module.ts
+++ basic/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { DemoModule } from './demo/demo.module';
-import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, DemoModule],
-  controllers: [AppController, UserController],
+  imports: [],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
