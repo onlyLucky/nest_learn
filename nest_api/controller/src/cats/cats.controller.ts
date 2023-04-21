9aaf547d52802f428baccda8433d3b9f2a2580a7
Feynman <2901954062@qq.com>
2023-04-21 16:33:09 +0800

    feat: 新增nest controller实例代码。

--- nest_api/controller/src/cats/cats.controller.ts
+++ nest_api/controller/src/cats/cats.controller.ts
@@ -2,6 +2,55 @@
  * @Author: fg
  * @Date: 2023-04-21 14:49:52
  * @LastEditors: fg
- * @LastEditTime: 2023-04-21 14:49:58
- * @Description: content
+ * @LastEditTime: 2023-04-21 16:25:17
+ * @Description: 控制器: 负责处理传入的请求和向客户端返回响应。
  */
+
+
+/* 
+@Controller() 装饰器定义一个基本的控制器
+*/
+import { Controller, Get, Req, Post, Body, HttpCode, Param, Put, Delete } from "@nestjs/common";
+// 获取express中的 Request类型的数据
+import { Request } from "express";
+import { CreateCatDto } from "./dto/create-cats.dto";
+
+/* 路由 */
+// 路由路径前缀设置为cats /cats
+@Controller('cats')
+export class CatsController {
+  /* 
+  @Get() HTTP 请求方法装饰器告诉 Nest 为 HTTP 请求的特定端点创建处理程序
+  Nest 会将 GET /cats 请求映射到此处理程序
+  @Get('profile') 组合会为 GET /cats/profile 请求生成路由映射
+  */
+  @Get()
+  @HttpCode(200)
+  findAll(@Req() request: Request): string {
+    return 'This action returns all cats'
+  }
+
+  @Post()
+  create(@Body() createCatDto: CreateCatDto): string {
+    return 'This action adds a new cat'
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return `This action updates a #${id} cat`
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action remove a #${id} cat`
+  }
+
+}
+
+// 要使用 CLI 创建控制器，只需执行 $ nest g controller cats
+
---
+++ nest_api/controller/src/cats/dto/create-cats.dto.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: fg
+ * @Date: 2023-04-21 16:21:05
+ * @LastEditors: fg
+ * @LastEditTime: 2023-04-21 16:21:14
+ * @Description: content
+ */
+export class CreateCatDto {
+  readonly name: string;
+  readonly age: number;
+  readonly breed: string;
+}
